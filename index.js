949bb1afbad6dfb1323852ce2e6f7a1da9209d01
“Ezgi <okurezgi@windowslive.com>
2022-05-03 16:25:09 +0300

    Done with phase-1-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase()
+    return customerName
+}
+
+function setBestCustomer(){
+    return bestCustomer= "not bob"
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer= "maybe bob"
+}
+
+const leastFavoriteCustomer = "noone"
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer="still noone"
+}
